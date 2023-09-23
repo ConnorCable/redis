961404c122f19fb3e8a7369650c0fc2d30ca44a0
ConnorCable <ccab0528@gmail.com>
2023-09-23 11:26:56 -0700

    worked on serialize_array

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,6 +1,7 @@
 #[allow(dead_code)]
 pub mod resp {
     use regex::Regex;
+    use tokio::net::windows::named_pipe::ServerOptions;
     use std::collections::VecDeque;
 
     #[derive(Debug, PartialEq, Eq)]
@@ -25,102 +26,172 @@ pub mod resp {
     }
 
     pub struct SerializedMessage {
-        pub length: u32,
+        pub length: usize,
         pub identifier: RespIdentifier,
         pub message: String,
     }
-    
-    fn ret_err_message(error: &str) -> SerializedContainer {
-    
-        let error_message = SerializedMessage {
-            length : error.len() as u32,
-            identifier : RespIdentifier::RespSimpleErr,
-            message : error.to_string()
-        };
 
-        let container = SerializedContainer {
-            length : 1,
-            messages: vec![error_message]
-        };
-
-        return container
+    trait UpdateLength {
+        fn update(&self, entry: SerializedMessage);
+        fn update_with_array(&self, entries: Vec<SerializedMessage>);
     }
 
-    pub fn serialize_array(message: &str, array_length: u32, ) -> SerializedContainer {
-        let container = SerializedContainer {
-            length: array_length,
-            messages : Vec::new(),
+    impl UpdateLength for SerializedContainer {
+        fn update(&self, entry: SerializedMessage) {
+            self.messages.push(entry);
+            self.length += 1;
+        }
+        fn update_with_array(&self, entries: Vec<SerializedMessage>) {
+            for entry in entries {
+                self.length += 1;
+                self.messages.push(entry)
+            }
+        }
+    }
+
+    fn ret_err_message(error: &str) -> SerializedMessage {
+        let error_message = SerializedMessage {
+            length: error.len(),
+            identifier: RespIdentifier::RespSimpleErr,
+            message: error.to_string(),
         };
 
-        let mut split: VecDeque<&str> = message.split("\r\n").filter(|x| !x.is_empty()).collect();
+        return error_message;
+    }
+    // receives
+    fn serialize_bulk_string(content: &str) -> SerializedMessage {
+        return SerializedMessage {
+            length: (content.len() - 2),
+            identifier: RespIdentifier::RespBulkStr,
+            message: content[2..].to_string(),
+        };
+    }
 
-        while !split.is_empty() {
-            if split.len() == 1 {
-                ret_err_message("Unpaired identifier / message");
-            }
-            let pair = split.pop_front().unwrap().split("");
-            let Some(message_identifier) = pair.get(0) {
-                
-            }
-            let entry = SerializedMessage {
-                identifier : detect_identifier(split.pop_front().unwrap()).unwrap(),
-                message: split.pop_front().unwrap().to_string(),
-                length: 
-            };
+    // splits an entry into its identifier and possible information
+    // if boolean is true -> the entry encapsulates everything about itself
+    // if boolean is false -> the entry informs the identity and length of the next entry
+    // eg: +OK returns (RespIdentifier:RespSimpleStr , OK, true)
+    // eg: $5 returns (RespIdentifier::RespBulkStr, 5, false)
+    fn entry_splitter(entry: &str) -> (RespIdentifier, String, bool) {
+        let mut split: Vec<&str> = entry.split("").filter(|x| !x.is_empty()).collect();
+        if split.len() < 2 {
+            
+        }
+
+        let identifier = detect_identifier(split.remove(0));
+
+        let message = split.concat();
+
+        match identifier {
+            RespIdentifier::RespInt => (identifier, message, true),
+            RespIdentifier::RespBigNumber => (identifier, message, true),
+            RespIdentifier::RespBool => (identifier, message, true),
+            RespIdentifier::RespSimpleStr => (identifier, message, true),
+            _ => (identifier, message, true),
         }
 
 
-        println!("{:?}", split);
 
-        return container
 
     }
+    // receives m
+    pub fn serialize_array(message: &str, length: &str) -> Vec<SerializedMessage> {
+        let arr_length: usize;
+        // TODO: NULL ARRAY
 
+        match length.parse::<usize>() {
+            Ok(n) => arr_length = n,
+            Err(e) => return vec![ret_err_message("Array length not parsed correctly!")],
+        }
+
+
+        let mut split: VecDeque<&str> = message.split("\r\n").filter(|x| !x.is_empty()).collect();
+        
+
+
+        let mut serialized_array = Vec::new();
+        // "*2\r\n$4\r\necho\r\n$5\r\nhello world\r\n”
+        // $4 , echo , $5 , hello world
+        // *3\r\n:1\r\n:2\r\n:3\r\n
+        // :1 , :2 , :3
+        while !split.is_empty() {
+            let (id , length_or_entry, encapsulates) = entry_splitter(split.pop_front().unwrap());
+            let serialized_message: SerializedMessage;
+            if encapsulates {
+                serialized_message = SerializedMessage {
+                    identifier : id,
+                    message : length_or_entry,
+                    length : length_or_entry.len()
+                };
+                serialized_array.push(serialized_message);
+                continue;
+            }
+            if let Ok(entry_length) = length_or_entry.parse::<usize>() {
+                let entry_message = split.pop_front().unwrap();
+                serialized_message = SerializedMessage {
+                    identifier : id,
+                    length : entry_length,
+                    message: split.pop_front().unwrap().to_string()
+                };
+                serialized_array.push(serialized_message);
+                continue;
+
+            }
+            else{
+                // if the parse isn't sucessful, discard the next 
+                split.pop_front();
+                serialized_array.push(ret_err_message("Error parsing length"))
+            }
+            
+
+        }
+
+        return serialized_array;
+    }
+    // receive the message
+    // if it is a simple message, process it with process_resp_simple
+    // if it is an array, process it with serialize_array
     pub fn construct_serialized_message(message: &str) -> SerializedContainer {
-        let mut split: Vec<&str> = message.split("").filter(|x| !x.is_empty()).collect();
         let mut container = SerializedContainer {
             length: 0,
             messages: Vec::new(),
         };
-        let Some(id) = detect_identifier(split.remove(0)) else {
-            println!("Could not detect identifier");
-            return ret_err_message("Could not detect identifier")
+        let identifier = detect_identifier(&message[0..1]);
+        match identifier {
+            RespIdentifier::RespArray => {
+                container.update_with_array(serialize_array(&message[2..], &message[1..1]))
+            }
+            RespIdentifier::RespBulkStr => container.update(serialize_bulk_string(message)),
+            RespIdentifier::RespInt | RespIdentifier::RespBigNumber => {
+                container.update(serialize_int(message))
+            }
+            RespIdentifier::RespBool
+            | RespIdentifier::RespSimpleStr
+            | RespIdentifier::RespSimpleErr => {
+                container.update(serialize_simple(&message[1..], identifier))
+            }
+            _ => container.update(ret_err_message("Identifier not parsed")),
         };
 
-        let Some(extracted) = process_resp_simple(message) else {
-            println!("Could not process simple message");
-            return ret_err_message("Could not process simple message")
-        };
-
-        let serializedmessage = SerializedMessage {
-            length: extracted.len() as u32,
-            identifier: id,
-            message: extracted.to_string(),
-        };
-
-        container.messages.push(serializedmessage);
-        container.length += 1;
         return container;
     }
 
-    fn detect_identifier(c: &str) -> Option<RespIdentifier> {
+    fn detect_identifier(c: &str) -> RespIdentifier {
         match c {
-            "+" => Some(RespIdentifier::RespSimpleStr),
-            "-" => Some(RespIdentifier::RespSimpleErr),
-            ":" => Some(RespIdentifier::RespInt),
-            "#" => Some(RespIdentifier::RespBool),
-            "*" => Some(RespIdentifier::RespArray),
-            "$" => Some(RespIdentifier::RespBulkStr),
-            _ => Some(RespIdentifier::RespSimpleErr),
+            "+" => RespIdentifier::RespSimpleStr,
+            "-" => RespIdentifier::RespSimpleErr,
+            ":" => RespIdentifier::RespInt,
+            "#" => RespIdentifier::RespBool,
+            "*" => RespIdentifier::RespArray,
+            "$" => RespIdentifier::RespBulkStr,
+            "-" => RespIdentifier::RespSimpleErr,
+            _ => RespIdentifier::RespSimpleErr,
         }
     }
     // captures the human readable string of the split message
-    pub fn process_resp_simple(message: &str) -> Option<&str> {
+    pub fn serialize_simple(message: &str, identifer: RespIdentifier) -> SerializedMessage {
         let capture_group = Regex::new(r".(.+)\r\n").unwrap();
         let captures = capture_group.captures(message);
-        match captures {
-            Some(x) => return Some(x.get(1).unwrap().as_str()),
-            None => return None,
-        }
     }
+    pub fn serialize_int(message: &str) -> SerializedMessage {}
 }
