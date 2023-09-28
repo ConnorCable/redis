07d1128372baf1d0f49bbb74d52d46051f7f961e
ConnorCable <ccab0528@gmail.com>
2023-09-28 06:44:52 -0700

    worked on server part

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 mod serialize;
-
-use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+use std::error::Error;
 use std::io;
 
-
 #[tokio::main]
-async fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Connect to a peer
+    let stream = TcpListener::bind("127.0.0.1:0").await?.accept().await
+
+    let mut msg = vec![0; 1024];
 
     loop {
-        let (_socket, _) = listener.accept().await?;
-        println!("ping!");
+        // Wait for the socket to be readable
+        stream.readable().await?;
+
+        // Try to read data, this may still fail with `WouldBlock`
+        // if the readiness event is a false positive.
+        match stream.try_read(&mut msg) {
+            Ok(n) => {
+                msg.truncate(n);
+                break;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
     }
 
-
-}
-
-async fn process<T> (_socket: T) {
-    println!("Ping!");
+    println!("GOT = {:?}", msg);
+    Ok(())
 }
